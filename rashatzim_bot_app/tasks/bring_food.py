c473780fc8163fd2410cfe382e46c0c9272f3b9d
Talor Abramovich <talor19@gmail.com>
2018-08-21 19:28:10 +0300

    for debugging

--- rashatzim_bot_app/tasks/bring_food.py
+++ rashatzim_bot_app/tasks/bring_food.py
@@ -1,8 +1,14 @@
 # encoding: utf-8
 from __future__ import unicode_literals
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
 
 
 def callback_minute(bot, job):
+    logger.info("starting callback for bring_food task, chat id: %d!", job.context)
     bot.send_message(chat_id=job.context, text='SPAM FOR SOUFI')
 
 
