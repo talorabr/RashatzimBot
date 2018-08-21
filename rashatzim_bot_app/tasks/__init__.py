b888e21fb7335c06c2f6487b60b847be69ac1f35
Talor Abramovich <talor19@gmail.com>
2018-08-21 22:22:14 +0300

    Fix

--- rashatzim_bot_app/tasks/__init__.py
+++ rashatzim_bot_app/tasks/__init__.py
@@ -15,5 +15,5 @@ def import_tasks(group):
     job_queue = updater.job_queue
     for taskname in ('bring_food',):
         task = getattr(importlib.import_module('rashatzim_bot_app.tasks.{taskname}'.format(taskname=taskname)), 'task')
-        logger.info('task imported: name=%s callback=%s interval=%d first=%d', task.name, task.callback[0], task.interval, task.first or 0)
+        logger.info('task imported: name=%s callback=%s interval=%s first=%s', task.name, task.callback[0], task.interval, task.first or 0)
         job_queue.run_repeating(callback=task.callback[0], interval=task.interval, first=task.first or 0, context=group.id)
